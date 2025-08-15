c9164d72cd6eb80205057779d263bd242989a77f
lxu <lxu@htc-login001.zib.de>
2025-08-15 17:45:31 +0200

    new results

--- jobs.sh
+++ jobs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Set variables
 timelimit=-1
-algorithms=("LDL" "D" "PPT") #("LDL" "D" "A" "LD1")
+algorithms=("A") #("LDL" "D" "A" "LD1")
 datapath="$PWD/benchmark"
 resultpath="$PWD/results"
 juliabin="julia" #"/home/lxu/software/julia-1.10.2/bin/julia"
