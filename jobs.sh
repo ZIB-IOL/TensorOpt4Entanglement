06d0a8848d78f9678f8c88051db4515302531958
lxu <lxu@htc-login001.zib.de>
2025-10-18 19:20:57 +0200

    add RLT results

--- jobs.sh
+++ jobs.sh
@@ -4,7 +4,7 @@ timelimit=-1
 algorithms=("RLT") #("RLT" "LD1" "LDR0" "LDR1" "LDR2" "LDR3" "LDR4" "LDR5" "LDL" "D" "A")
 datapath="$PWD/benchmark"
 resultpath="$PWD/results"
-juliabin="julia" #"/home/lxu/software/julia-1.10.2/bin/julia"
+juliabin="/home/lxu/software/julia-1.10.2/bin/julia"
 
 
 # Generate all job combinations and save to file
@@ -13,9 +13,9 @@ for instance in $(ls $datapath)
 do
   for algorithm in "${algorithms[@]}"
   do
-      if ! grep -q "N = 5" "$datapath/$instance"; then
-        continue
-      fi
+      #if ! grep -q "N = 5" "$datapath/$instance"; then
+      #  continue
+      #fi
       echo  "-s" "$instance" "-a" "$algorithm" "-t" "$timelimit"
       echo "$juliabin" "$instance" "$algorithm" "$timelimit">> job_list.txt
   done
